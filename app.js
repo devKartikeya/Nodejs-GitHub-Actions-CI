55b9e872128f7e8fcd500ae4662f964e8725c358
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-08-02 20:44:59 +0530

    Add automated unit testing with Jest

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+function add(a, b) {
+    return a + b;
+}
 
-// Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Basic route
-app.get('/', (req, res) => {
-  res.send('Hello GitHub Actions!');
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = { add };
